4fd99c28fe46b80c011692a8677153a8d224a835
ozturkomerfaruk <56068905+ozturkomerfaruk@users.noreply.github.com>
2022-12-29 21:17:00 +0300

    bitirme

--- Bitirme Projesi/NeworlD/NeworlD/App/Utils/Constants.swift
+++ Bitirme Projesi/NeworlD/NeworlD/App/Utils/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     //Please create your own API_KEY.
-    static let API_KEY = "5c88fb184b054a5d9c989205c02111a2" //"##API_KEY##"
+    static let API_KEY = "##API_KEY##"
     
     //If an image is nil, i want to use this image on the Internet. But we can upload an image and use it here too. I just wanted to use an image on the internet.
     static let OPPSImageURL = "https://thumbs.dreamstime.com/z/oops-sticky-note-yellow-pinned-red-push-pin-cork-board-92680037.jpg"
